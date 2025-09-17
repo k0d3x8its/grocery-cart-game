a2d737382a26a2d83025ab446b3c094ecfd6e2ba
K0d3x 8its <k0d3x@pm.me>
2025-09-16 21:17:17 -0400

    added Global.gd

---
+++ autoload/Global.gd
@@ -0,0 +1,22 @@
+extends Node
+
+signal score_changed(new_score)
+signal game_ended
+
+var score := 0
+var playing := true
+
+func reset():
+	score = 0
+	playing = true
+	emit_signal("score_changed", score)
+
+func add_score(n:int):
+	if not playing: return
+	score += n
+	emit_signal("score_changed", score)
+
+func game_over():
+	if not playing: return
+	playing = false
+	emit_signal("game_ended")
