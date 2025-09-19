dddb8e8cbb5bc2970f92b4000c327681102887aa
K0d3x 8its <k0d3x@pm.me>
2025-09-19 11:45:54 -0400

    added signal to area that will catch items

---
+++ scenes/cart.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+@export var speed := 900.0
+var drag_active := false
+var drag_offset := Vector2.ZERO
+
+func _unhandled_input(event):
+	if event is InputEventScreenTouch:
+		drag_active = event.pressed
+		if event.pressed: drag_offset = global_position - event.position
+	elif event is InputEventScreenDrag and drag_active:
+		global_position.x = (event.position + drag_offset).x
+	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+		drag_active = event.pressed
+		if event.pressed: drag_offset = global_position - event.position
+	elif event is InputEventMouseMotion and drag_active:
+		global_position.x = (event.position + drag_offset).x
+
+func _physics_process(_d):
+	var dir := 0.0
+	if Input.is_action_pressed("move_left"):  dir -= 1.0
+	if Input.is_action_pressed("move_right"): dir += 1.0
+	velocity.x = dir * speed
+	if dir != 0.0: move_and_slide()
+
+	var vw := get_viewport_rect().size.x
+	global_position.x = clamp(global_position.x, 60.0, vw - 60.0)
+
+func _on_catch_zone_area_entered(area: Area2D) -> void:
+	if area.is_in_group("item"):
+		Global.add_score(1)
+		area.queue_free()
+	elif area.is_in_group("mascot"):
+		Global.game_over()
