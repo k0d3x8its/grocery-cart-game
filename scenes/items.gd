8bb8d313d1b0a4de00d262336dd33f24caad1035
K0d3x 8its <k0d3x@pm.me>
2025-09-25 17:56:04 -0400

    added mascot recognition

--- scenes/items.gd
+++ scenes/items.gd
@@ -1,7 +1,10 @@
 extends Area2D
 @export var fall_speed := 500.0
 
-func _process(delta):
+func _process(delta: float) -> void:
+	if not Global.playing:
+		return
 	position.y += fall_speed * delta
+	
 	if position.y > get_viewport_rect().size.y + 100.0:
 		queue_free()
