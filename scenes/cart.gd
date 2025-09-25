26ac4b51a2bee1a2f0befc1c6cf1f35474be9ceb
K0d3x 8its <k0d3x@pm.me>
2025-09-25 17:50:29 -0400

    added mascot group and _on_game_ended function

--- scenes/cart.gd
+++ scenes/cart.gd
@@ -4,7 +4,12 @@ extends CharacterBody2D
 var drag_active := false
 var drag_offset := Vector2.ZERO
 
+func _ready() -> void:
+	Global.game_ended.connect(_on_game_ended)
+
 func _unhandled_input(event):
+	if not Global.playing:
+		return
 	if event is InputEventScreenTouch:
 		drag_active = event.pressed
 		if event.pressed: drag_offset = global_position - event.position
@@ -17,6 +22,9 @@ func _unhandled_input(event):
 		global_position.x = (event.position + drag_offset).x
 
 func _physics_process(_d):
+	if not Global.playing:
+		velocity.x = 0.0
+		return
 	var dir := 0.0
 	if Input.is_action_pressed("move_left"):  dir -= 1.0
 	if Input.is_action_pressed("move_right"): dir += 1.0
@@ -32,3 +40,7 @@ func _on_catch_zone_area_entered(area: Area2D) -> void:
 		area.queue_free()
 	elif area.is_in_group("mascot"):
 		Global.game_over()
+		
+func _on_game_ended() -> void:
+	drag_active = false
+	velocity = Vector2.ZERO
