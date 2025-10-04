d1cce65dfc2c08d9c06e26ea09edf1388f45ad5c
K0d3x 8its <k0d3x@pm.me>
2025-10-04 19:39:10 -0400

    refactored code to be more legible and added Scoring API

--- scripts/cart.gd
+++ scripts/cart.gd
@@ -1,15 +1,18 @@
 extends CharacterBody2D
 
 @export var speed := 1200.0
-var drag_active := false
-var drag_offset := Vector2.ZERO
+@export var edge_padding_pixels: float = 50.0
+
+var drag_active: bool = false
+var drag_offset: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	Global.game_ended.connect(_on_game_ended)
 
-func _unhandled_input(event):
+func _unhandled_input(event) -> void:
 	if not Global.playing:
 		return
+		
 	if event is InputEventScreenTouch:
 		drag_active = event.pressed
 		if event.pressed: drag_offset = global_position - event.position
@@ -21,24 +24,32 @@ func _unhandled_input(event):
 	elif event is InputEventMouseMotion and drag_active:
 		global_position.x = (event.position + drag_offset).x
 
-func _physics_process(_d):
+func _physics_process(_delta_seconds: float) -> void:
 	if not Global.playing:
 		velocity.x = 0.0
 		return
-	var dir := 0.0
-	if Input.is_action_pressed("move_left"):  dir -= 1.0
-	if Input.is_action_pressed("move_right"): dir += 1.0
-	velocity.x = dir * speed
-	if dir != 0.0: move_and_slide()
+		
+	var input_direction: float = 0.0
+	if Input.is_action_pressed("move_left"):  input_direction -= 1.0
+	if Input.is_action_pressed("move_right"): input_direction += 1.0
+	
+	velocity.x = input_direction * speed
+	if input_direction != 0.0: move_and_slide()
 
-	var vw := get_viewport_rect().size.x
-	global_position.x = clamp(global_position.x, 60.0, vw - 60.0)
+	var viewport_width: float = get_viewport_rect().size.x
+	global_position.x = clamp(global_position.x, edge_padding_pixels, viewport_width - edge_padding_pixels)
 
-func _on_catch_zone_area_entered(area: Area2D) -> void:
-	if area.is_in_group("item"):
-		Global.add_score(1)
-		area.queue_free()
-	elif area.is_in_group("mascot"):
+func _on_catch_zone_area_entered(entered_area: Area2D) -> void:
+	if not Global.playing:
+		return
+
+	if entered_area.is_in_group("item"):
+		var score_change_from_catch: int = 1
+		if entered_area.has_method("get_score_on_catch"):
+			score_change_from_catch = entered_area.get_score_on_catch()
+		Global.add_score(score_change_from_catch)
+		entered_area.queue_free()
+	elif entered_area.is_in_group("mascot"):
 		Global.game_over()
 		
 func _on_game_ended() -> void:
